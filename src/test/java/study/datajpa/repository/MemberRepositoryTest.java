a871b3271773226464e1962e266fcf1e3986eaa1
Lee  Chanho <sma05091@nate.com>
2023-09-03 12:03:32 +0900

    스프링 데이터 JPA 페이징 count query분리, 페이지를 유지하면서 엔티티를 DTO로 변환하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -139,7 +139,12 @@ public class MemberRepositoryTest {
 
         //when
         PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+
         Page<Member> page = memberRepository.findByAge(10, pageRequest);
+        Page<MemberDto> dtoPage = page.map(m -> new MemberDto(m.getId(), m.getUsername(),null));
+
+
 
         //then
         List<Member> content = page.getContent(); //조회된 데이터
@@ -149,6 +154,8 @@ public class MemberRepositoryTest {
         assertThat(page.getTotalPages()).isEqualTo(2); //전체 페이지 수
         assertThat(page.isFirst()).isTrue(); // 첫번째 항복인가?
         assertThat(page.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+
 
     }
 }
